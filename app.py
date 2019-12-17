87444154d66a3c0b1f045115822ac2fee7bb68ed
Daniel <danielohayon444@gmail.com>
2019-12-17 10:58:35 +0200

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import instabot
-bot = instabot.Bot(filter_users=False)
-bot.login(username='beautifullyfitpeople', password='Daniel979')
-bot.follow("ronaldinho")
+# import instabot
+# bot = instabot.Bot(filter_users=False)
+# bot.login(username='beautifullyfitpeople', password='Daniel979')
+# bot.follow("ronaldinho")
 print("fadsfadsfdasfasdfas")
